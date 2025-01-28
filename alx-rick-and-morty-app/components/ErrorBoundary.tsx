cdc1b2457e835c5b76e078d8ecb9966540a9ac73
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2025-01-28 17:22:41 +0100

    Mastering Error Handling

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/react';
+
 interface State {
   hasError: boolean;
 }
@@ -18,7 +20,7 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps , State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log({ error, errorInfo });
+    Sentry.captureException(error, { extra: errorInfo });
   }
 
   render() {
